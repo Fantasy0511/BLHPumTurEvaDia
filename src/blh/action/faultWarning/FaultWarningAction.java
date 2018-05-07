24a4a31a6af4524f041978744122988f2afbf2a3
tiandiwei <38423318+tiandiwei@users.noreply.github.com>
2018-05-07 14:33:45 +0800

    完善故障预警模块

---
+++ src/blh/action/faultWarning/FaultWarningAction.java
@@ -0,0 +1,50 @@
+package blh.action.faultWarning;
+
+import java.util.List;
+
+import org.apache.struts2.convention.annotation.Result;
+
+import blh.action.support.AbstractActionSupport;
+import service.FaultInfoService;
+import tool.easyui.Table;
+import util.FaultUtils;
+import util.TimeUtils;
+
+/**
+ * 故障预警查询
+ * 
+ * @author tiandiwei
+ *
+ */
+
+@Result(type="json")
+public class FaultWarningAction extends AbstractActionSupport {
+
+	private static final long serialVersionUID = 1L;
+	private String system;
+	private long beginDate;
+	private long endDate;
+	private Table historyTable;
+
+	@Override
+	public String execute() throws Exception {
+		system = getFirstInput();
+		beginDate = TimeUtils.StringtoLong(getSecondInput() + "00:00:00");
+		endDate = TimeUtils.StringtoLong(getThirdInput() + "00:00:00");
+		System.out.println(system + "," + beginDate);
+		FaultInfoService faultInfoService = new FaultInfoService();
+		List<FaultUtils> data = faultInfoService.getFaultInfos(system, beginDate, endDate);
+		String[] headers = { "故障名称", "关联参数" };
+		historyTable = new Table(headers);
+		for (FaultUtils fUtils : data) {
+			historyTable.withRow(fUtils);
+		}
+
+		return super.execute();
+	}
+
+	public Table getHistoryTable() {
+		return historyTable;
+	}
+
+}
