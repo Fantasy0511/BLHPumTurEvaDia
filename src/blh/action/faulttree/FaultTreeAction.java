052332cb8df58bf26dd348cb30333335fef0635c
tiandiwei <38423318+tiandiwei@users.noreply.github.com>
2018-05-24 10:57:03 +0800

    故障树页面样式更改

---
+++ src/blh/action/faulttree/FaultTreeAction.java
@@ -0,0 +1,12 @@
+package blh.action.faulttree;
+
+import blh.action.support.AbstractActionSupport;
+/**
+ * 前端调用时间和故障id 返回故障id和故障描述
+ * @author tiandiwei
+ *
+ */
+public class FaultTreeAction extends AbstractActionSupport{
+	
+
+}
