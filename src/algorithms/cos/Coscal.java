8bb4eea681af6d5abf0c72150670345ba632b94b
lh <403077115@qq.com>
2018-07-03 15:58:09 +0800

    cos输出每个系统的最大故障概率

--- src/algorithms/cos/Coscal.java
+++ src/algorithms/cos/Coscal.java
@@ -18,12 +18,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import com.opensymphony.xwork2.inject.util.Strings;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import bll.diagnosis.tree.model.pumInitialNodes;
 import algorithms.fpgrowth.DataDiscrete;
 import dao.impl.ReadData;
 import service.FaultInfoService;
 import util.DataUtils;
 import util.FaultUtils;
 import util.PathUtil;
+import util.TimeUtils;
 
 /**
  * 余弦相似度计算
@@ -40,7 +45,7 @@ public class Coscal {
 	/*生成本地文件的，用过一次就行了
 	public Coscal(){
 		try {
-			File f = new File("src/vectors.txt");
+			File f = new File("src/vectors1.txt");
 			OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(f),"gbk");      
 			this.rd = new ReadData();
 			this.allfaults= this.rd.queFault();
@@ -48,15 +53,18 @@ public class Coscal {
 			this.disc = new DataDiscrete();
 			this.len = StringMap.size();
 			for(int i=0;i<allfaults.size();i++){
-				long starttime = allfaults.get(i).getStarttime();
-				long endtime = allfaults.get(i).getEndtime();
-				ArrayList<DataUtils> datas = this.rd.queAllRecord(starttime,endtime);
-				float[] vector = dataUtilstoList(datas,this.StringMap);
-				faultVectors.add(vector);
-				for(int j=0;j<vector.length;j++){
-					out.write(""+(int) vector[j]+" ");
+				if(allfaults.get(i).getFaultID()<100){
+					long starttime = allfaults.get(i).getStarttime();
+					long endtime = allfaults.get(i).getEndtime();
+					ArrayList<DataUtils> datas = this.rd.queAllRecord(starttime,endtime);
+					float[] vector = dataUtilstoList(datas,this.StringMap);
+					faultVectors.add(vector);
+					for(int j=0;j<vector.length;j++){
+						out.write(String.valueOf(vector[j])+" ");
+						//out.write(""+(int) vector[j]+" ");
+					}
+					out.write("\r\n");
 				}
-				out.write("\r\n");
 			}
 			out.close();
 		} catch (ClassNotFoundException e) {
@@ -82,7 +90,7 @@ public class Coscal {
 				String[] vectorStrings = line.split(" ");
 				float[] vector = new float[vectorStrings.length];
 				for(int i=0;i<vectorStrings.length;i++){
-					vector[i] = Float.parseFloat(vectorStrings[i]); 
+					vector[i] = Float.parseFloat(vectorStrings[i]);
 				}
 				faultVectors.add(vector);
 			}
@@ -91,6 +99,7 @@ public class Coscal {
 			this.StringMap = this.rd.queAlltype();
 			this.disc = new DataDiscrete();
 			this.len = StringMap.size();
+			bf.close();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -102,6 +111,9 @@ public class Coscal {
 			e.printStackTrace();
 		} 
 	}
+	/**
+	 * 计算两个向量间余弦相似度
+	 * */
 	private double getCos(float[] a,float[] b){
 		double product_ab = 0,product_a=0,product_b=0;
 		for(int i=0;i<len;i++){
@@ -112,7 +124,9 @@ public class Coscal {
 		return product_ab/(Math.pow(product_a, 0.5)*Math.pow(product_b, 0.5));
 	}
 	
-	
+	/**
+	 * 将读取到的数据转换为向量
+	 * */
 	private float[] dataUtilstoList(ArrayList<DataUtils> datas,HashMap<String, Integer> StringMap) {
 		float[] rs = new float[this.len];
 		for(int i=0;i<datas.size();i++){
@@ -132,9 +146,80 @@ public class Coscal {
 		
 	}
 	
-	public  ArrayList<Double> getSimilarityDegree(String starttime,String endtime){
+	/**
+	 * 将隶属于每个故障样本的按照子系统聚类
+	 * */
+	private HashMap<String, float[]> cluster() {
+		HashMap<String, float[]> sys_vectors = new HashMap<String, float[]>();
+		HashMap<String, Integer> sys_counters = new HashMap<String, Integer>();
+		String[] keys = {"水泵水轮机","调速器系统","发电机及励磁系统","主变系统","球阀系统"};
+		for(int i=0;i<keys.length;i++){
+			sys_vectors.put(keys[i], new float[len]);//特征故障向量
+			sys_counters.put(keys[i], 0);//向量计数器
+		}
+		for(int i=0;i<allfaults.size();i++){
+			String sys = allfaults.get(i).getSystem();
+			sys_counters.put(sys, sys_counters.get(sys)+1);
+			float[] tmp_vector = sys_vectors.get(sys);
+			for(int j=0;j<len;j++){
+				tmp_vector[j] += faultVectors.get(i)[j];
+			}
+			sys_vectors.put(sys, tmp_vector);
+		}
+		for(int i=0;i<keys.length;i++){
+			int counter = sys_counters.get(keys[i]);
+			float[] tmp_vector = sys_vectors.get(keys[i]);
+			for(int j=0;j<len;j++){
+				tmp_vector[j] = tmp_vector[j]/counter;
+			}
+			sys_vectors.put(keys[i], tmp_vector);
+		}
+		return sys_vectors;
+	}
+	
+	/**
+	 * 获得开始结束时间内的向量，并与5大系统的故障向量对比，获得每个相似的概率
+	 * */
+	private  HashMap<String, Double> getSimilarityDegreeOfSystemgs(String starttime,String endtime){
+		HashMap<String, Double> sys_pro = new HashMap<String, Double>();
+		HashMap<String, Integer> sys_counters = new HashMap<String, Integer>();
+		String[] keys = {"水泵水轮机","调速器系统","发电机及励磁系统","主变系统","球阀系统"};
+		for(int i=0;i<keys.length;i++){
+			sys_pro.put(keys[i], 0.0);
+			sys_counters.put(keys[i], 0);
+		}
+		ArrayList<Double> rs = this.getSimilarityDegree(starttime,endtime);
+		for(int i=0;i<rs.size();i++){
+			String sys = allfaults.get(i).getSystem();
+			double cos = rs.get(i);
+			sys_pro.put(sys,sys_pro.get(sys)>cos?sys_pro.get(sys):cos);//存储最靠近的概率
+			/*sys_pro.put(sys, sys_pro.get(sys)+cos); //概率累加*/
+			sys_counters.put(sys,sys_counters.get(sys)+1);
+		}
+		/*
+		 * 如果是概率累计的话，则需要除以总数量
+		for(int i=0;i<keys.length;i++){
+			double cos = sys_pro.get(keys[i]);
+			sys_pro.put(keys[i], cos/sys_counters.get(keys[i]));
+		}
+		*/
+		//求相对值
+		double total = 0;
+		for(int i=0;i<keys.length;i++){
+			total += sys_pro.get(keys[i]);
+		}
+		for(int i=0;i<keys.length;i++){
+			sys_pro.put(keys[i], sys_pro.get(keys[i])/total);
+		}
+		return sys_pro;
+	}
+	
+	/**
+	 * 获得开始结束时间内的向量，并与已知所有的故障向量对比，获得每个相似的概率
+	 * */
+	private  ArrayList<Double> getSimilarityDegree(String starttime,String endtime){
 		ArrayList<Double> rs = new ArrayList<Double>();
-		double total = 0.0,cos;
+		double cos;
 		try {
 			ArrayList<DataUtils> datas = this.rd.queAllRecord(starttime, endtime);
 			float[] vector = dataUtilstoList(datas,StringMap);
@@ -143,11 +228,7 @@ public class Coscal {
 				if(Double.isNaN(cos)){
 					cos = 0;
 					}
-				total = total + cos;
 				rs.add(cos);
-			}
-			for(int i=0;i<faultVectors.size();i++){
-				rs.set(i, rs.get(i)/total);
 			}
 			
 		} catch (ClassNotFoundException | SQLException e) {
@@ -157,11 +238,16 @@ public class Coscal {
 		return rs;
 	}
 	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		Coscal cc = new Coscal();
-		String starttime="2014-01-01 00:00:00";
-		String endtime="2014-01-02 00:00:00";
-		ArrayList<Double> rs = cc.getSimilarityDegree(starttime, endtime);
-		System.out.print(rs);
+		String starttime="2015-05-11 07:20:00";
+		String endtime="2015-05-11 08:20:00";
+		String date = TimeUtils.LongtoString(1431303600L);
+		HashMap<String, Double> pro_systems = cc.getSimilarityDegreeOfSystemgs(starttime, endtime);
+		System.out.print(pro_systems);
 	}
 }
