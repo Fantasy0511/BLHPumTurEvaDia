3175a8a0a0ce4ed053e53423f5c870f387e78fc6
chenxinhong1234 <wswsdtk@163.com>
2018-09-08 17:28:40 +0800

    添加输入时间判断功能，判断时间是否是数据库已有表的时间

--- src/blh/action/faulttree/FaultPumTreeAction.java
+++ src/blh/action/faulttree/FaultPumTreeAction.java
@@ -1,10 +1,13 @@
 package blh.action.faulttree;
 
 import java.util.List;
+
 import org.apache.struts2.convention.annotation.Result;
+
 import blh.action.support.AbstractActionSupport;
 import bll.diagnosis.tree.model.FaultResult;
 import service.faulttree.PumFaultTreeService;
+import tool.exception.JudgeTime;
 import tool.ui.easyui.Table;
 
 /**
@@ -21,12 +24,17 @@ public class FaultPumTreeAction extends AbstractActionSupport {
 	private int[] resultNode;
 	private String[] columnHeaders = { "faultName", "probality", "describe" };// 故障描述表
 	Table faultInfoTable = new Table(columnHeaders);
-
+	private String judgeResult;
+	
 	@Override
 	public String execute() throws Exception {
 		String time = getFirstInput() + " 00:00:00";
 		untiNo=getSecondInput();
 		System.out.println("输出时间和机组号："+time+"   "+untiNo);
+		
+		// 判断输入的时间是否能在数据库中找到相应表格
+		JudgeTime jt = new JudgeTime();
+		judgeResult = jt.judgeTime(time);
 		
 		results = new PumFaultTreeService().findfault(time,untiNo);
 		resultNode = new int[results.size()];
@@ -73,5 +81,14 @@ public class FaultPumTreeAction extends AbstractActionSupport {
 	public void setFaultInfoTable(Table faultInfoTable) {
 		this.faultInfoTable = faultInfoTable;
 	}
+
+	public String getJudgeResult() {
+		return judgeResult;
+	}
+
+	public void setJudgeResult(String judgeResult) {
+		this.judgeResult = judgeResult;
+	}
+
 
 }
