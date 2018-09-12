92774d9225a8c8f6e392ddc742b017bf7d8b5374
Asus <Asus@windows10.microdone.cn>
2018-09-12 15:14:33 +0800

    枚举类型更改

--- src/blh/action/predict/itemConvert.java
+++ src/blh/action/predict/itemConvert.java
@@ -2,8 +2,8 @@ package blh.action.predict;
 
 public enum itemConvert {
 //枚举类型转换
-	B14B("527"), LQLL("331"),SW("1076"), YWEI("309"),U18V("525"),
-	LG0C("1097"), B144("777"), B145("778"), B146("779");	
+	B14B("308"), LQLL("331"),SW("1076"), YWEI("309"),U18V("306"),
+	LG0C("1097"), B144("328"), B145("329"), B146("330");	
 	
 	private String system;
 
